c446b6e83d95f493b4c86906a3b395ea629626ca
Brihan Bocanegra <brihan.bocanegra@outlook.com>
2021-10-23 13:09:49 -0500

    Agregar archivos de proyecto.

---
+++ WebApp2021/Controllers/MedicamentoController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApp2021.Controllers
+{
+    public class MedicamentoController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Inicio()
+        {
+            return View();
+        }
+
+        public string Saludo()
+        {
+            return "Hola Mundo";
+        }
+
+        public string SaludoNombreApellido(string nombre, string apellido)
+        {
+            return "¡Hola " + nombre + " " + apellido + "!";
+        }
+
+
+    }
+}
