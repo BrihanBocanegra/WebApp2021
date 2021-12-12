d7bb18d41ce31da569ea101f290d3f1639eaa271
Brihan Bocanegra <brihan.bocanegra@outlook.com>
2021-12-11 21:04:07 -0500

    DESARROLLO WEB CON ASP.NET - SESIÓN 03

---
+++ WebApp2021/Controllers/LaboratorioController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CapaNegocio;
+using CapaEntidad;
+
+namespace WebApp2021.Controllers
+{
+    public class LaboratorioController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public List<LaboratorioCLS> ListarLaborario()
+        {
+            LaboratorioBL obj = new LaboratorioBL();
+            return obj.ListarLaboratorio();
+        }
+    }
+}
